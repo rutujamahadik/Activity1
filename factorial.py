be1ba0d48371113e24aecb3d192b56064f854f19
unknown <rutujamahadik1204@gmail.com>
2022-02-08 17:47:00 +0530

    factorial file

---
+++ factorial.py
@@ -0,0 +1,7 @@
+
+fact = 1
+
+for i in range(1,n+1):
+    fact = fact * i
+
+ print("Factorial  = ",fact)
--- sum of series/sum_of_series.py
+++ sum of series/sum_of_series.py
@@ -3,4 +3,8 @@ sum = 0
 
 for i in range(1,n+1):
     sum = sum+i
+fact = 1
+
+for i in range(1,n+1):
+    fact = fact * i
 print("Sum of the no is:",sum)
